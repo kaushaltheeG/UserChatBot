ab44e625818067f58e54d7c7fdc9c9aeff004fe4
kaushaltheeG <kaushalsk09@gmail.com>
2023-06-13 09:15:11 -0700

    created and styled content view

--- UserChatBotPOC/ContentView.swift
+++ UserChatBotPOC/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showAlert: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack(spacing: 20) {
+                NavigationLink(destination: UsersIndex(), label: {
+                    Text("Chat with User")
+                        .font(.headline)
+                        .padding()
+                        .frame(maxWidth: 300)
+                        .foregroundColor(.purple)
+                        .background(Color.purple.opacity(0.5))
+                        .cornerRadius(30)
+                        .accessibilityIdentifier("navigationToUsersIndex")
+                })
+                
+                Button(action: {
+                    showAlert.toggle()
+                }, label: {
+                    Text("Show Alert!")
+                        .font(.headline)
+                        .padding()
+                        .frame(maxWidth: 300)
+                        .foregroundColor(.red)
+                        .background(Color.red.opacity(0.4))
+                        .cornerRadius(30)
+                })
+                .accessibilityIdentifier("showAlertButton")
+                .alert(isPresented: $showAlert, content: {
+                    return Alert(title: Text("Alert!!!"))
+                })
+            }
+            .padding()
+            .navigationTitle("Chat Bot Home")
         }
-        .padding()
+        
     }
 }
 
