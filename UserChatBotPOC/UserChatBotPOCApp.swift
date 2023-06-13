51f820d68440690cd2296b02157d2fdfe1ea3f88
kaushaltheeG <kaushalsk09@gmail.com>
2023-06-12 19:09:12 -0700

    first commit

---
+++ UserChatBotPOC/UserChatBotPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserChatBotPOCApp.swift
+//  UserChatBotPOC
+//
+//  Created by Kaushal Kumbagowdana on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct UserChatBotPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
